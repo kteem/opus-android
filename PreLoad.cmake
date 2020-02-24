2f2f409796aeb9f37d969f81b47be5d7d14bd9ff
Kim Tae-wook <kteem84@gmail.com>
2020-02-24 16:56:18 +0900

    initial commit

---
+++ PreLoad.cmake
@@ -0,0 +1,4 @@
+set(
+  CMAKE_TOOLCHAIN_FILE "${CMAKE_CURRENT_LIST_DIR}/polly/android-ndk-r21-api-24-arm64-v8a-clang-libcxx11.cmake"
+  CACHE PATH "toolchain"
+  )
---
+++ build/CMakeFiles/3.16.2/CMakeSystem.cmake
@@ -0,0 +1,24 @@
+set(CMAKE_HOST_SYSTEM "Darwin-19.2.0")
+set(CMAKE_HOST_SYSTEM_NAME "Darwin")
+set(CMAKE_HOST_SYSTEM_VERSION "19.2.0")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/Users/kteem/Documents/Projects/opus-android/polly/android-ndk-r21-api-24-arm64-v8a-clang-libcxx11.cmake")
+
+set(CMAKE_SYSTEM "Android-24")
+set(CMAKE_SYSTEM_NAME "Android")
+set(CMAKE_SYSTEM_VERSION "24")
+set(CMAKE_SYSTEM_PROCESSOR "aarch64")
+
+set(CMAKE_ANDROID_NDK "/Users/kteem/Library/Android/sdk/ndk/21.0.6113669")
+set(CMAKE_ANDROID_STANDALONE_TOOLCHAIN "")
+set(CMAKE_ANDROID_ARCH "arm64")
+set(CMAKE_ANDROID_ARCH_ABI "arm64-v8a")
+set(CMAKE_ANDROID_ARCH_TRIPLE "aarch64-linux-android")
+set(CMAKE_ANDROID_NDK_DEPRECATED_HEADERS "0")
+set(CMAKE_ANDROID_NDK_TOOLCHAIN_HOST_TAG "darwin-x86_64")
+set(CMAKE_ANDROID_NDK_TOOLCHAIN_UNIFIED "/Users/kteem/Library/Android/sdk/ndk/21.0.6113669/toolchains/llvm/prebuilt/darwin-x86_64")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
